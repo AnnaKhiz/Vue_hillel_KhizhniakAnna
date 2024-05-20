e0a5317cd1717213dde62132ffbed27a9bb08cc2
annakhizhniak <kukurika.anna@gmail.com>
2024-05-20 14:55:17 +0300

    HW 2. NPM

---
+++ index.js
@@ -0,0 +1,15 @@
+function calcSum(a,b) {
+	return a + b;
+}
+
+function calcSubtract(a,b) {
+	return a - b;
+}
+
+function calcMult(a,b) {
+	return a * b;
+}
+
+calcSum(1,2);
+calcSubtract(4,1);
+calcMult(4,5);
