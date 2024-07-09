3c0a16e97d5d79233ad46e92ee27ea9c59774bc3
annakhizhniak <kukurika.anna@gmail.com>
2024-07-09 14:04:17 +0300

    del store

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import router from "../router";
-import store from "../store"
 
 const app = createApp(App)
 
 app
-  .use(store)
   .use(router)
   .mount('#app')
